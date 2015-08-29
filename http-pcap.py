3ab9f955fe7e0a7be4c87dfaf3c924cb6e0a8eb0
liusy1128 <liusy1128@sina.com>
2015-08-29 08:53:12 +0800

    Update http-pcap.py
    add the input ，code into function

--- http-pcap.py
+++ http-pcap.py
@@ -1,41 +1,29 @@
-
+#encoding=utf-8
+# -*- coding:gb2312 -*-
 import dpkt
 import socket
 import time
 import testdb
+import statistics
+import os.path
+import string
 
-#from testdb.py import opendata
-#from testdb.py import insert
-#from testdb.py import showalldata
 
-print 'import pcap file,please wait'
-#f = open('F:/python/pcap-test.pcap','rb')
-f = open('F:/python/http-pcap2.pcap','rb')
 
-pcap = dpkt.pcap.Reader(f)
-
-i = 1 #±¨ÎÄ±àºÅ£¬¼ÇÂ¼wiresharkÖĞµÄĞòºÅ£¬±ãÓÚµ÷ÊÔ
-firsttime = 0
-lasttime = 0
-tabel_line = {}  #Êı¾İ¿âĞĞ´æ´¢½á¹¹
-cur = testdb.opendata()  #Êı¾İ¿âµÄconn
-conn = cur[1]
-not_ip_packet = 0  #¼ÇÂ¼×¥È¡µÄ±¨ÎÄÖĞ·Çip°üµÄ¸öÊı
-not_tcp_packet = 0 #¼ÇÂ¼×¥È¡µÄ±¨ÎÄÖĞ·Çtcp°üµÄ¸öÊı
-
+# ç§’è½¬åŒ–ä¸ºæ—¥æœŸ
 def timeformat_sec_to_date(timestamp):
     timeArray = time.localtime(timestamp)
     otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
     return otherStyleTime
 
 #input_firsttime = str(raw_input("firsttime"))
-
+#æ—¥æœŸè½¬åŒ–ä¸ºå¦™
 def timeformat_date_to_sec(timestamp):
     tup_birth = time.strptime(timestamp, "%Y-%m-%d %H:%M:%S");
     birth_secds = time.mktime(tup_birth)
     return birth_secds
 
-#ÔİÊ±Ö»¶Ô²¿·ÖÍøÖ·½øĞĞ½âÎö
+#å¯¹urlè¿›è¡Œæ•´å½¢ï¼Œæš‚æ—¶åªå¯¹éƒ¨åˆ†ç½‘å€è¿›è¡Œè§£æ
 def urlformat(url):
     err = 0
     test = url.find('.net') #http://blog.chinaunix.net
@@ -53,11 +41,11 @@ def urlformat(url):
     test = url.find('.com')#www.baidu.com
     if test != -1 :
         cnnum = url.find('.com.cn') #www.sina.com.cn
-        if cnnum == -1: #.comÍøÖ·
+        if cnnum == -1: #.comç½‘å€
             num = test+4
-        elif test == cnnum: #.com.cnÍøÖ·
+        elif test == cnnum: #.com.cnç½‘å€
             num = test+7
-        else:#ÔÚ.comÍøÖ·ÖĞ·ÃÎÊÁË.com.cnÍøÖ·£¬È¡µÚÒ»¸ö
+        else:#åœ¨.comç½‘å€ä¸­è®¿é—®äº†.com.cnç½‘å€ï¼Œå–ç¬¬ä¸€ä¸ª
             num = test+4
         format_url = url[0:num]
         return format_url,err
@@ -71,102 +59,176 @@ def urlformat(url):
     err = 1
     return url,err
 
-for ts,buf in pcap:
-    #¼ÇÂ¼µÚÒ»¸ö±¨ÎÄÊ±¼ä
-    if i == 1:
-        firsttime = ts
 
-    eth = dpkt.ethernet.Ethernet(buf)
-    if eth.type!=2048:
-        #print 'not ip packet %d'%i
-        not_ip_packet =  not_ip_packet+1
-        i= i+1
-        continue
-         
-    ip = eth.data
-    if ip.p != 6:
-        #print 'not tcp packet %d'%i
-        not_tcp_packet = not_tcp_packet + 1
-        i= i+1
-        continue
-    tcp = ip.data
+#f = open('F:/python/pcap-test.pcap','rb')
+
+
+firsttime = 0
+lasttime = 0
+tabel_line = {}  #æ•°æ®åº“è¡Œå­˜å‚¨ç»“æ„
+
+def packet_import_to_db():
+    not_ip_packet = 0  #è®°å½•æŠ“å–çš„æŠ¥æ–‡ä¸­éipåŒ…çš„ä¸ªæ•°
+    not_tcp_packet = 0 #è®°å½•æŠ“å–çš„æŠ¥æ–‡ä¸­étcpåŒ…çš„ä¸ªæ•°
+    f = open('F:/python/http-pcap2.pcap','rb')
     
-    #if tcp.dport == 80 and len(tcp.data) > 0:
-    if len(tcp.data) > 0:
-        #print 'packet num %d'%i
-        if tcp.dport == 80 :
-            try:
-                http = dpkt.http.Request(tcp.data)
-            except:
-               
-                i = i+1
-                continue
-            find = 0
-            #print '===================================='
-            for k,v in http.headers.iteritems():
-                if k == 'referer':
-                    find = 1
-                    break
-           
-            if find != 1:
-                for k,v in http.headers.iteritems():
-                    if k == 'origin':
-                        break
- 
+    try:
+        pcap = dpkt.pcap.Reader(f)
+    except:
+        f.close()
+        return
+    
+    cur = testdb.opendata()  #æ•°æ®åº“çš„conn
+    conn = cur[1]
+    i = 1#æŠ¥æ–‡ç¼–å·ï¼Œè®°å½•wiresharkä¸­çš„åºå·ï¼Œä¾¿äºè°ƒè¯•
+    for ts,buf in pcap:
+        #è®°å½•ç¬¬ä¸€ä¸ªæŠ¥æ–‡æ—¶é—´
+        if i == 1:
+            firsttime = ts
 
-            tabel_line['timestamp'] = ts
-            tabel_line['sip'] = socket.inet_ntoa(ip.src)
-            tabel_line['dip'] = socket.inet_ntoa(ip.dst)
-            tabel_line['sport'] = tcp.sport
-            tabel_line['dport'] = tcp.dport
-            tabel_line['method'] = http.method
-            url= urlformat(v)
-            tabel_line['url'] = url[0]
-            
-            tabel_line['tcp_packet'] = tcp.data
-            
-
-            #for k, value in  tabel_line.items():
-            #    print k,value
-            #ÓĞĞ§µÄurl²åÈë
-            if url[1] == 0: 
-                testdb.insert(tabel_line,conn)
-                
-            tabel_line.clear()
-
-        if tcp.sport == 80 :
-            try:
-                http = dpkt.http.Response(tcp.data)
-                #print http.reason
-                #print http.status
-                #print http.version
-            except:
-                #print 'response err'
-                i = i+1
-                continue
-            #for k,v in http.headers.iteritems():
-            #        print '%s:%s' % (k, v)
-                    
-    #else :
+        eth = dpkt.ethernet.Ethernet(buf)
+        if eth.type!=2048:
+            #print 'not ip packet %d'%i
+            not_ip_packet =  not_ip_packet+1
+            i= i+1
+            continue
+             
+        ip = eth.data
+        if ip.p != 6:
+            #print 'not tcp packet %d'%i
+            not_tcp_packet = not_tcp_packet + 1
+            i= i+1
+            continue
+        tcp = ip.data
         
-        #if i==19:
-        #    print tcp.dport
-        #    print len(tcp.data)
-        #    print 'not http packet %d'%i
+        #if tcp.dport == 80 and len(tcp.data) > 0:
+        if len(tcp.data) > 0:
+            #print 'packet num %d'%i
+            if tcp.dport == 80 :
+                try:
+                    http = dpkt.http.Request(tcp.data)
+                except:
+                   
+                    i = i+1
+                    continue
+                find = 0
+                #print '===================================='
+                for k,v in http.headers.iteritems():
+                    if k == 'referer':
+                        find = 1
+                        break
+               
+                if find != 1:
+                    for k,v in http.headers.iteritems():
+                        if k == 'origin':
+                            break
+     
 
-    i = i+1
-#¼ÇÂ¼×îºóÒ»¸ö±¨ÎÄÊ±¼ä
-lasttime = ts
-testdb.closedata(conn)
-f.close()
+                tabel_line['timestamp'] = ts
+                tabel_line['sip'] = socket.inet_ntoa(ip.src)
+                tabel_line['dip'] = socket.inet_ntoa(ip.dst)
+                tabel_line['sport'] = tcp.sport
+                tabel_line['dport'] = tcp.dport
+                tabel_line['method'] = http.method
+                url= urlformat(v)
+                tabel_line['url'] = url[0]
+                
+                tabel_line['tcp_packet'] = tcp.data
+                
 
-print 'the databse'
-#testdb.showalldata()
+                #for k, value in  tabel_line.items():
+                #    print k,value
+                #æœ‰æ•ˆçš„urlæ’å…¥
+                if url[1] == 0: 
+                    testdb.insert(tabel_line,conn)
+                    
+                tabel_line.clear()
+            #é‡ç‚¹å…³æ³¨å®¢æˆ·æŠ¥æ–‡ï¼Œç½‘é¡µå†…å®¹æš‚ä¸å…³æ³¨
+            if tcp.sport == 80 :
+                try:
+                    http = dpkt.http.Response(tcp.data)
+                except:
+                    #print 'response err'
+                    i = i+1
+                    continue      
+        #else :
+            
+            #if i==19:
+            #    print tcp.dport
+            #    print len(tcp.data)
+            #    print 'not http packet %d'%i
 
-print 'this pcap file pcap packet from %s to %s'%(timeformat_sec_to_date(firsttime),timeformat_sec_to_date(lasttime))
-print 'firsrtime %d,last time %d'%(firsttime,lasttime)
+        i = i+1
+        if i == 500:
+            print 'please wait a moment'
+    #è®°å½•æœ€åä¸€ä¸ªæŠ¥æ–‡æ—¶é—´
+    lasttime = ts
+    testdb.closedata(conn)
+    f.close()
+
+    print 'this pcap file pcap packet from %s to %s'%(timeformat_sec_to_date(firsttime),timeformat_sec_to_date(lasttime))
+    print 'read file finish'
+
+def url_make_cmdStr():
+    print 'this pcap file pcap packet from %s to %s'%(timeformat_sec_to_date(firsttime),timeformat_sec_to_date(lasttime))
+    whileflag = True
+
+    while flag :
+        print 'please input the start time ,for example: 2015-08-23 17:11:57'
+        tempStr = 'start should after %s\r\n'%timeformat_sec_to_date(firsttime)
+        startime_date = str(raw_input(tempStr))
+        startime_input_sec = timeformat_date_to_sec(startime_date)
+
+        print 'please input the end time,for example: 2015-08-23 17:11:57 '
+        tempStr = 'end should before %s\r\n'%timeformat_sec_to_date(lasttime)
+        endtime_date = str(raw_input(tempStr))
+        endtime_input_sec = timeformat_date_to_sec(endtime_date)
 
 
+        if (endtime_input_sec >  startime_input_sec) and (startime_input_sec > firsttime) and (endtime_input_sec < lasttime) :
+            break
+        else :
+            print 'input err,again\r\n'
+    
+    cmsStr = "select * from http_packet where timestamp > %d and timestamp < %d"%(startime_input_sec,endtime_input_sec)
+    return cmsStr
+
+    
+
+if __name__ == '__main__':
+    #å¯ä»¥åšæˆè®©ç”¨æˆ·è¾“å…¥æ–‡ä»¶åï¼Œæ­¤å¤„ç®€å•å¤„ç†
+    filename="http-pcap-data3.db"
+    #å¦‚æœæŠ¥æ–‡å·²ç»è¯»å–ï¼Œç›´æ¥è¯»æ•°æ®åº“ï¼Œæ²¡æœ‰åˆ™è§£æpcapæŠ¥æ–‡
+    if True == os.path.exists(filename):
+       firsttime =  testdb.GetMin_timestamp(filename)
+       lasttime = testdb.GetMax_timestamp(filename)
+       print 'this pcap file have been saved in DB'
+       #print 'this pcap file pcap packet from %s to %s'%(timeformat_sec_to_date(firsttime),timeformat_sec_to_date(lasttime))
+    else : 
+        print 'read pcap file to database,please wait'
+        #packet_import_to_db()
 
 
+    flag = True
+    while flag:
+        print 'please select which function you want'
+        print '1.sort the url record between time'
+        print '2.find the keyword in http packet'
+        print '3.exit'
+        choice = str(raw_input())
+        if choice == '1':
+            cmdStr = url_make_cmdStr()
+           
+            statistics.url_Statistics(filename,cmdStr)
+        elif choice == '2':
+            keyword = str(raw_input('please input the keyword '))
+            n = str(raw_input('please input the print item num '))
+            statistics.keyword_statistcis(filename,keyword,string.atoi(n))
+        else :
+            break
+            
+
+    
+        
+    
 
